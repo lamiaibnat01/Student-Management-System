d04b3623dd8379b5ef6cd984ea9a85e5a9571ea8
lamiaibnat01 <lamia.ibnat01@gmail.com>
2026-06-08 22:02:04 +0600

    Add grade calculation function based on marks

---
+++ modules/grades.py
@@ -0,0 +1,19 @@
+def calculate_grade(mark):
+
+    if mark >= 80:
+        return "A+"
+
+    elif mark >= 70:
+        return "A"
+
+    elif mark >= 60:
+        return "A-"
+
+    elif mark >= 50:
+        return "B"
+
+    elif mark >= 40:
+        return "C"
+
+    else:
+        return "F"
